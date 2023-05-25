9cc315d3531c0483e4ce4dbacfc0cb1cfd055692
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-25 14:31:55 +0300

    create main-layout component

--- src/app/lms/lms.module.ts
+++ src/app/lms/lms.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LmsRoutingModule } from './lms-routing.module';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainLayoutComponent
+  ],
   imports: [
     CommonModule,
     LmsRoutingModule
