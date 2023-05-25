02663e2e07cef6c9938160a2c4a71707a822df71
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-25 14:51:22 +0300

    add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAQ1RK1IrI-TC5W3FUCJE28-kgTk1xtnOs",
+    authDomain: "lms-demo-66222.firebaseapp.com",
+    projectId: "lms-demo-66222",
+    storageBucket: "lms-demo-66222.appspot.com",
+    messagingSenderId: "359171685821",
+    appId: "1:359171685821:web:44e364b18227fb5e3466d4",
+    measurementId: "G-60RTC9EB92"
+  }
 };
 
 /*
