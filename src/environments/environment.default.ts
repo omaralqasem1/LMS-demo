4785c754e5e560f72af1a0dd6d4e515b3486f553
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-25 15:22:01 +0300

    initialize firbase config

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,11 @@
+export const defaultEnvironment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyAQ1RK1IrI-TC5W3FUCJE28-kgTk1xtnOs",
+        authDomain: "lms-demo-66222.firebaseapp.com",
+        projectId: "lms-demo-66222",
+        storageBucket: "lms-demo-66222.appspot.com",
+        messagingSenderId: "359171685821",
+        appId: "1:359171685821:web:44e364b18227fb5e3466d4",
+        measurementId: "G-60RTC9EB92"
+    }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { defaultEnvironment } from "./environment.default";
+
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAQ1RK1IrI-TC5W3FUCJE28-kgTk1xtnOs",
-    authDomain: "lms-demo-66222.firebaseapp.com",
-    projectId: "lms-demo-66222",
-    storageBucket: "lms-demo-66222.appspot.com",
-    messagingSenderId: "359171685821",
-    appId: "1:359171685821:web:44e364b18227fb5e3466d4",
-    measurementId: "G-60RTC9EB92"
-  }
+  ...defaultEnvironment
 };
 
 /*
