b6c6af5381d90c7617150f4233f3bbffaeef88d9
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-25 15:23:43 +0300

    import BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire/compat'
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { environment } from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
