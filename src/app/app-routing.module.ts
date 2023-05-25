cf9b886d99d45c7f8d02d4ad8027be81efdc91cf
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-25 14:26:28 +0300

    create lms module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./lms/lms.module').then(m => m.LmsModule)
+  },
+  {
     path: 'account',
     loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
   }
---
+++ src/app/lms/lms.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LmsRoutingModule } from './lms-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LmsRoutingModule
+  ]
+})
+export class LmsModule { }
