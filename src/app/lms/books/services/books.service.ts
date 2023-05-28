127143a04521a9153821a1ea30998e6ff515ed6c
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-28 19:15:21 +0300

    create books-service

---
+++ src/app/lms/books/services/books.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, QueryFn } from '@angular/fire/compat/firestore';
+import { IBook } from '../models/book.interface';
+import { from, map } from 'rxjs';
+import { collection, getCountFromServer } from 'firebase/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  private _collectionName: string;
+
+  constructor(private fireStore: AngularFirestore) {
+    this._collectionName = 'books';
+  }
+
+  create(book: IBook) {
+    const id = this.fireStore.createId();
+    return from(this.fireStore.doc(`${this._collectionName}/${id}`).set({ ...book, id }));
+  }
+
+  get(id: string) {
+    return this.fireStore.collection<IBook>(this._collectionName).doc(id).valueChanges();
+  }
+
+  getCount() {
+    const collectionRef = collection(this.fireStore.firestore, this._collectionName);
+    return from(getCountFromServer(collectionRef)).pipe(
+      map(res => res.data().count)
+    );
+  }
+
+  getList(queryFn?: QueryFn) {
+    return this.fireStore.collection<IBook>(this._collectionName, queryFn).valueChanges();
+  }
+
+  delete(id: string) {
+    return from(this.fireStore.collection<IBook>(this._collectionName).doc(id).delete());
+  }
+
+  update(book: IBook) {
+    return from(this.fireStore.collection<IBook>(this._collectionName).doc(book.id).update(book));
+  }
+}
