b5f0a56b30bcc305351571638599021703a504a5
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-28 19:51:20 +0300

    show count of books

--- src/app/lms/components/home/home.component.ts
+++ src/app/lms/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../../categories/services/categories.service';
 import { Observable } from 'rxjs';
+import { BooksService } from '../../books/services/books.service';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,11 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
   categoriesCount$: Observable<number>;
-  constructor(private categoriesService: CategoriesService) {
-    this.categoriesCount$ = this.categoriesService.getCount();
+  booksCount$: Observable<number>;
+
+  constructor(categoriesService: CategoriesService, booksService: BooksService) {
+    this.categoriesCount$ = categoriesService.getCount();
+    this.booksCount$ = booksService.getCount();
   }
 
   ngOnInit(): void {
