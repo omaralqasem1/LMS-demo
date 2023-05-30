9eac9b023208e91fec8815f76248b80de119e8a0
omaralqasem1 <omaralqasem59@hotmail.com>
2023-05-30 23:25:46 +0300

    handle in progress

--- src/app/lms/categories/components/create-category/create-category.component.ts
+++ src/app/lms/categories/components/create-category/create-category.component.ts
@@ -12,21 +12,37 @@ import { Category } from '../../models.ts/category.class';
 })
 export class CreateCategoryComponent implements OnInit {
   nameCtrl: FormControl;
+  inProgress: boolean;
+  hasError: boolean;
 
   constructor(
     public dialogRef: MatDialogRef<CreateCategoryComponent>,
     private categoriesService: CategoriesService
   ) {
     this.nameCtrl = new FormControl('', Validators.required);
+    this.inProgress = false;
+    this.hasError = false;
   }
 
   ngOnInit(): void {
   }
 
   create(): void {
+    if (this.nameCtrl.invalid || this.inProgress) return;
+    this.hasError = false;
+    this.inProgress = true;
     const category = new Category(this.nameCtrl.value);
-    this.categoriesService.create(category).subscribe(_ => {
-      this.dialogRef.close(category);
-    });
+    this.categoriesService.create(category).subscribe(
+      {
+        next: _ => {
+          this.inProgress = false;
+          this.dialogRef.close(category);
+        },
+        error: _ => {
+          this.hasError = true;
+          this.inProgress = false;
+        }
+      }
+    );
   }
 }
